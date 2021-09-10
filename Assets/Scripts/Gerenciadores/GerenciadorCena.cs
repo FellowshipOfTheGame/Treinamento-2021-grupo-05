04795219df7b5b6d1ce026239c756abace845c80
joseccarmo <carmoj272@gmail.com>
2021-09-10 11:19:17 -0300

    Menu atualizado
    menu e script para high sccore

---
+++ Assets/Scripts/Gerenciadores/GerenciadorCena.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GerenciadorCena : MonoBehaviour
+{
+    public static GerenciadorCena instancia;
+    private GerenciadorSom gerenciadorSom;
+
+    private void Awake()
+    {
+        if (instancia == null)
+        {
+            instancia = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instancia != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        gerenciadorSom = GerenciadorSom.instancia;
+    }
+
+    public void IniciarGameScene()
+    {
+        gerenciadorSom.TrocarMusica("boss1");
+        gerenciadorSom.TocarEfeito("botao");
+        SceneManager.LoadScene("BulletHell");
+    }
+}
---
+++ Assets/Scripts/Gerenciadores/GerenciadorSom.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerenciadorSom : MonoBehaviour
+{
+    public static GerenciadorSom instancia;
+
+    private float musicaVolume = 1f;
+    private float SFXVolume = 1f;
+
+    private AudioClip creditos, menu, botao, boss1, boss2, boss3; // musicas
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        if (instancia == null)
+        {
+            instancia = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instancia != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        creditos = Resources.Load<AudioClip>("Creditos");
+        menu = Resources.Load<AudioClip>("Trilhasonorapause");
+        botao = Resources.Load<AudioClip>("04-Som Select, stop, next");
+        boss1 = Resources.Load<AudioClip>("Boss trilha 1 150 bpm fá maior");
+        boss2 = Resources.Load<AudioClip>("Boss trilha 1 150 bpm fá maior");
+        boss3 = Resources.Load<AudioClip>("Boss trilha 1 150 bpm fá maior");
+
+        audioSource.volume = musicaVolume;
+    }
+
+    public void TocarEfeito(string efeito)
+    {
+
+        switch (efeito)
+        {
+            case "botao":
+                audioSource.PlayOneShot(botao, SFXVolume);
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    public void TrocarMusica(string musica)
+    {
+        switch (musica)
+        {
+            case "menu":
+                audioSource.clip = menu;
+                audioSource.Play();
+                break;
+            case "creditos":
+                audioSource.clip = creditos;
+                audioSource.Play();
+                break;
+            case "boss1":
+                audioSource.clip = boss1;
+                audioSource.Play();
+                break;
+            case "boss2":
+                audioSource.clip = boss2;
+                audioSource.Play();
+                break;
+            case "boss3":
+                audioSource.clip = boss3;
+                audioSource.Play();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void TrocarMusicaBoss(string musica)
+    {
+        float tempo = audioSource.time;
+        switch (musica)
+        {
+            case "boss1":
+                audioSource.clip = boss1;
+                audioSource.time = tempo;
+                audioSource.Play();
+                break;
+            case "boss2":
+                audioSource.clip = boss2;
+                audioSource.time = tempo;
+                audioSource.Play();
+                break;
+            case "boss3":
+                audioSource.clip = boss3;
+                audioSource.time = tempo;
+                audioSource.Play();
+                break;
+            default:
+                break;
+        }
+
+    }
+    public void MudarVolumeMusica(float volume)
+    {
+        audioSource.volume = volume;
+        PlayerPref.SetMusicVolume(volume);
+    }
+
+    public void MudarVolumeSFX(float volume)
+    {
+        SFXVolume = volume;
+        PlayerPref.SetSFXVolume(volume);
+
+    }
+}
