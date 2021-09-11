69d56808fbfe95fd4a3b538859dbbdfd1657210c
RodrigoLimaRFL <guico21@usp.br>
2021-09-11 15:40:07 -0300

    a

--- Assets/Scripts/Inimigo/Respawn.cs
+++ Assets/Scripts/Inimigo/Respawn.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Respawn : MonoBehaviour
 {
     public int numInimigosMortos;
+    [SerializeField] private Text atual;
+    [SerializeField] private Text high;
     public bool isMorto = false;
     [SerializeField] private GameObject inimigo;
     [SerializeField] private Transform pontoRespawn;
     [SerializeField] private Transform destino;
+    [SerializeField] private GerenciadorSom gerenciadorScript;
+    [SerializeField] private Score scoreScript;
     private Posicao_Tiros posicaoScript;
     private Movimento_Inimigo movimentoScript;
     private Laser laserScript;
@@ -17,8 +22,17 @@ public class Respawn : MonoBehaviour
     private Inimigo inimigoScript;
     private bool isParado = true;
 
+
     void Start()
     {
+        scoreScript.Carregar();
+
+        Score.atualScore = numInimigosMortos;
+        atual.text = Score.atualScore.ToString();
+        high.text = Score.highScore.ToString();
+
+        gerenciadorScript.TrocarMusica("boss1");
+
         posicaoScript = inimigo.GetComponent<Posicao_Tiros>();
         movimentoScript = inimigo.GetComponent<Movimento_Inimigo>();
         inimigoScript = inimigo.GetComponent<Inimigo>();
@@ -51,6 +65,16 @@ public class Respawn : MonoBehaviour
         inimigo.transform.position = pontoRespawn.position;
 
         isParado = false;
+
+        Score.atualScore = numInimigosMortos;
+
+        if (Score.highScore < numInimigosMortos)
+            Score.highScore = numInimigosMortos;
+
+        scoreScript.Salvar();
+
+        atual.text = Score.atualScore.ToString();
+        high.text = Score.highScore.ToString();
 
         yield return new WaitForSeconds(5f);
 
